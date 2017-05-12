d496670fe6f542ef124da7e582e217c02ea2b813
BLayman <leasure57@gmail.com>
2017-05-12 13:00:13 -0600

    first commit basic functionality

---
+++ Public/client.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+  var socket = io(); // our socket io connection
+
+  // event listeners
+
+  // listen for code sent from server
+  socket.on("sendBack", function (code) {
+    // append code to posts
+    $("#posts").append("<pre class = \"post\">"+ code +"</pre>");
+  });
+
+  // when a user joins
+  socket.on('join', function (codeList) {
+    // populate their posts using codeList from server
+    codeList.forEach(function (block) {
+      $("#posts").append("<pre class = \"post\">"+ block +"</pre>");
+    });
+  });
+
+  // when posts are to be cleared
+  socket.on('cleared',function functionName() {
+    // remove all posts
+    $('#posts').children().remove();
+  });
+
+  // click events
+
+  // when you click on a post
+  $(document).on("click", ".post", function(event) {
+    event.preventDefault();
+    // display post content in the main view
+    $('#displayCode').html($(this).html());
+  });
+
+  // when code is submitted
+  $('#submitCode').click(function(event) {
+    event.preventDefault();
+    // get content from text area
+    var code = $('#txtArea').val();
+    // send content to server
+    socket.emit('sendCode',code);
+    // empty text area
+    $('#txtArea').val("");
+  });
+
+  // when clear submissions is clicked
+  $('#clear').click(function(event) {
+    event.preventDefault();
+    // confirm
+    if(confirm("Are you sure you want to delete all submissions?")){
+      // tell server to clear codeList, and notify other users
+      socket.emit('clear');
+    }
+  });
+
+});
---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var codeList = []; // for storing code submissions
+// send files in Public folder
+app.use(express.static("Public"));
+/*
+// rendering practice
+app.get('/posts',function (req,res) {
+  res.locals = {submissions : codeList};
+  res.render('posts.ejs');
+});
+*/
+
+// io connection
+io.on('connection',function(socket){
+
+  console.log("connected");
+  // emit join event
+  socket.emit('join', codeList);
+
+  // listen for code sent from user
+  socket.on('sendCode', function (code) {
+    console.log(code);
+    // add to codeList array
+    codeList.push(code);
+    // broadcast code to all users
+    socket.emit('sendBack', code);
+    socket.broadcast.emit('sendBack', code);
+  });
+  // listen for event to clear code submissions
+  socket.on('clear',function () {
+    // empty codeList array
+    codeList = [];
+    // tell browsers to clear content
+    socket.emit('cleared');
+    socket.broadcast.emit('cleared');
+  });
+
+});
+
+server.listen(8080);
